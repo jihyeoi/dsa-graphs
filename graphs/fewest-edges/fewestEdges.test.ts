9476bbce19cbf141e6b56bf2d05a101451b9ff7a
Jazz Cheema <jaskamalc@gmail.com>
2024-04-25 15:46:07 -0700

    on last exercise

--- graphs/fewest-edges/fewestEdges.test.ts
+++ graphs/fewest-edges/fewestEdges.test.ts
@@ -8,17 +8,17 @@ describe("fewestEdges", function () {
     //         /  |  \
     //        I - T - H
     //        |        |
-    //        A        M   X
+    //                 M   X
 
     const graph = new UGraphStr();
     const [r, i, t, h, m, x] = graph.addFromVals(
         ["r", "i", "t", "h", "m", "x"]);
     graph.addEdges([[r, i], [i, t], [t, h], [r, t], [r, h], [h, m]]);
-    expect(fewestEdges(r, r)).toBe(0);
-    expect(fewestEdges(r, i)).toBe(1);
-    expect(fewestEdges(r, t)).toBe(1);
+    // expect(fewestEdges(r, r)).toBe(0);
+    // expect(fewestEdges(r, i)).toBe(1);
+    // expect(fewestEdges(r, t)).toBe(1);
     // expect(fewestEdges(r, h)).toBe(1);
-    // expect(fewestEdges(r, m)).toBe(2);
+    expect(fewestEdges(r, m)).toBe(2);
     // expect(fewestEdges(i, m)).toBe(3);
     // expect(fewestEdges(r, x)).toBe(Infinity);
   });
--- graphs/fewest-edges/fewestEdges.ts
+++ graphs/fewest-edges/fewestEdges.ts
@@ -5,66 +5,30 @@ import { Queue } from "../common/queue";
  * If no path can be found, return Infinity. */
 
 function fewestEdges(start: UGraphNodeStr, sought: UGraphNodeStr): number {
+  let currentQueue = new Queue([start]); // [i, t, h]
+  let seen = new Set([start]); // [r, i, t, h]
 
-  let current = new Queue([start])
-  let seen = new Set([start])
+  if (start.value === sought.value) return 0;
 
-  let count = 0;
+  let count = 0; //1
 
-  while (!current.isEmpty()) {
-    let node = current.dequeue();
-    if (node.value === sought.value) return count
+  while (!currentQueue.isEmpty()) {
+    let currentNode = currentQueue.dequeue(); // i
+    seen.add(currentNode);
 
-    console.log("all nodes?", node.adjacent)
-    let row = [node.adjacent]
-
-    if (row.has(sought)) {
+    if (currentNode.value === sought.value) {
       return count + 1;
     }
 
-    else {
-      // add to seen
-      for (let node of row) {
-        seen.add(node.value);
+    for (let neighbor of currentNode.adjacent) {
+      if (!seen.has(neighbor)) {
+        currentQueue.enqueue(neighbor);          // [i, t, h]
+        seen.add(neighbor);                     // [i, t, h]
+        count++;                               // 1
+        console.log("here");
       }
-
-      // get length of row
-      let length = row.length
-
-      //
-
-      for (let node of row) {
-        row.add(node.adjacent)
-      }
-      // dequeue all of them and then
-      // put the adjacent properties into a new row
-      // check that row
-      // count ++;
     }
-
-    for (let n of row) {
-      console.log("n", n)
-
-      // if (!seen.has(n)) {
-
-        // group the row
-        // go through the whole thing
-        // if its there then add 1 and return
-        // if not there then next adjacents
-
-        // count ++;
-
-        // console.log('n: ', n.value)
-        // console.log('sought: ', sought.value)
-
-        // if (n.value === sought.value) return count
-
-        // seen.add(n)
-        // current.enqueue(n);
-        // console.log('n: ', n.value)
-        // console.log('count: ', count)
-      // }
-    }
+    count--;
   }
 
   return Infinity;
